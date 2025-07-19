510f0bd86aecfa684c7774d8bb1e862594e3eb08
mxzmr <mxzmr@proton.me>
2025-07-19 08:52:37 +0300

    Initial Commit

---
+++ WithEdgeTests/WithEdgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WithEdgeTests.swift
+//  WithEdgeTests
+//
+//  Created by Max zam on 19/07/2025.
+//
+
+import Testing
+@testable import WithEdge
+
+struct WithEdgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
